8785e0c65634ef0a61572919b0a3492447ecc77e
MrFICAX <79323284+MrFICAX@users.noreply.github.com>
2021-03-18 14:56:52 +0100

    dodajem

---
+++ Domaci1/src/index.ts
@@ -0,0 +1,10 @@
+import { Movie } from "./movies";
+
+console.log("Hello world");
+
+const Movies= [];
+Movies.push(new Movie("Spiderman", 2005, 8));
+Movies.push(new Movie("Spiderman2", 2023, 9));
+Movies.push(new Movie("Spiderman3", 2015, 10));
+
+console.log(Movies);
