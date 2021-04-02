e78d3847f824f1c123aa40cda49169361de2e280
MrFICAX <79323284+MrFICAX@users.noreply.github.com>
2021-04-02 14:48:34 +0200

    Update

--- Domaci3 RXJS/src/Prognoza.ts
+++ Domaci3 RXJS/src/Prognoza.ts
@@ -3,11 +3,13 @@ export class Prognoza{
     private temperatura:number;
     private vreme:string;
     private mesto:string;
+    private datum:Date;
 
-    constructor(temp:number, vreme:string, mesto:string){
+    constructor(temp:number, vreme:string, mesto:string, datum:Date){
         this.setTemp(temp);
         this.setVreme(vreme);
         this.setMesto(mesto);
+        this.setDatum(datum);
     }
     setMesto(mesto: string) {
        this.mesto = mesto;
@@ -18,6 +20,12 @@ export class Prognoza{
     }
     setVreme(vrednost:string){
         this.vreme = vrednost;
+    }
+    setDatum(vrednost:Date){
+        this.datum = vrednost;
+    }
+    getDatum():Date{
+        return this.datum;
     }
     getTemp():number{
        return this.temperatura; 
--- Domaci3 RXJS/src/index.ts
+++ Domaci3 RXJS/src/index.ts
@@ -1,6 +1,5 @@
 import { interval, Observable, range, Subject } from "rxjs";
-import { filter, map, take, takeUntil } from "rxjs/operators";
-import * as Rx from "rxjs";
+//import { filter, map, take, takeUntil } from "rxjs/operators";
 import { Prognoza } from "./Prognoza";
 
 let NizVremena: string[] = ["ladno", "toplo", "vruce", "zima", "prohladno"];
@@ -15,22 +14,49 @@ let NIzLokacija: string[] = [
 ];
 
 const subjectA = new Subject();
+CrtajKontrole();
+
+let SavTekst = document.body.querySelector(".tekst");
 
 // subscriber 1
 subjectA.subscribe((data: Prognoza) => {
   console.log("SUBSCRIBER 1");
+  console.log("Subjektivni osecaj je: " + data.getVreme() + "<br>");
 
-  console.log("Subjektivni osecaj je: " + data.getVreme());
+  SavTekst.innerHTML += "SUBSCRIBER 1 <br>";
+  SavTekst.innerHTML +=
+    "Subjektivni osecaj je: " + data.getVreme() + "<br> <br>";
 });
 
 // subscriber 2
 subjectA.subscribe((data: Prognoza) => {
   console.log("SUBSCRIBER 2");
   console.log("Trenutna temperatura je: " + data.getTemp() + "*C");
+
+  SavTekst.innerHTML += "SUBSCRIBER 2 <br>";
+  SavTekst.innerHTML +=
+    "Trenutna temperatura je: " + data.getTemp() + "<br> <br>";
 });
+
+// subscriber 3
 subjectA.subscribe((data: Prognoza) => {
   console.log("SUBSCRIBER 3");
   console.log("Lokacija je: " + data.getMesto());
+
+  SavTekst.innerHTML += "SUBSCRIBER 3 <br>";
+  SavTekst.innerHTML += "Lokacija je: " + data.getMesto() + "<br> <br>";
+});
+
+// subscriber 4
+subjectA.subscribe((data: Prognoza) => {
+  console.log("SUBSCRIBER 4");
+  console.log("Datum je: " + data.getDatum().toLocaleString());
+
+  SavTekst.innerHTML += "SUBSCRIBER 4 <br>";
+  SavTekst.innerHTML +=
+    "Datum je: " + data.getDatum().toLocaleString() + "<br>";
+  SavTekst.innerHTML +=
+    "____________________________________________________________ <br> <br>";
 });
 
 function PokreniUnos(uneto: number, granica: number) {
@@ -41,13 +67,15 @@ function PokreniUnos(uneto: number, granica: number) {
       //subjectA.complete();
       break;
     }
-
     console.log(x);
+    SavTekst.innerHTML += "NOVA VREMENSKA PROGNOZA!!! <br> Redni broj: "+x+"<br>";
+
     subjectA.next(
       new Prognoza(
-        Math.random() * 100,
+        (Math.random() * 100) % 50,
         NizVremena[x % NizVremena.length],
-        NIzLokacija[x % NIzLokacija.length]
+        NIzLokacija[x % NIzLokacija.length],
+        new Date()
       )
     );
   }
@@ -83,8 +111,19 @@ function CrtajKontrole() {
       alert("Proveri konzolu!");
     } else alert("Niste uneli validne podakte");
   };
+
+  let divElement = document.createElement("div");
+  document.body.appendChild(divElement);
+
+  let naslov = document.createElement("h1");
+  divElement.appendChild(naslov);
+  naslov.innerHTML = "Podaci o Vremenskoj prognozi";
+
+  labela = document.createElement("label");
+  divElement.appendChild(labela);
+  labela.className = "tekst";
+  labela.innerHTML = "____________________________________________________________<br>";
 }
-CrtajKontrole();
 
 //   const observable = Rx.Observable.create((observer: Subject<unknown>) => {
 //     observer.next(Math.random());
